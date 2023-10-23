a00f0028748489e5c1d5e22c0fba0a0047967d66
clarkbarrett101 <clarkbarrett101@gmail.com>
2023-10-23 15:06:23 -0600

    when there are multiple screens, only the first plays music

--- Logical_Layer/Players/a3c_observer.py
+++ Logical_Layer/Players/a3c_observer.py
@@ -6,35 +6,37 @@ class A3C_Observer(Observer):
     def __init__(self, game_instance):
         super().__init__(game_instance)
         self.player_x = 0
+        self.enemies_per_column = [0] * 11
         self.bullet_x = []
         self.average_enemy_x = 0
-        self.space_bot = Space_bot.Space_bot(5, 5, self)
-        self.space_bot.set_inputs([0.0] * 3)
-        self.space_bot.set_layers()
-        self.start = True
-    def update(self, px, enemies, bullets):
+        self.Space_bot = Space_bot.space_bot(3,3,self)
+        self.Space_bot.set_inputs([0.0] * 3)
+        self.Space_bot.set_layers()
+
+    def update(self):
         self.player_x = self.game_instance.player.rect.x
+        self.enemies_per_column = [0] * 11
         self.bullet_x = []
-        self.space_bot.score = self.game_instance.score
         for enemy in self.game_instance.enemies:
+            self.enemies_per_column[enemy.column] += 1
             self.average_enemy_x += enemy.rect.x
-        self.average_enemy_x /= len(self.game_instance.enemies)+1
-        for bullet in self.game_instance.enemyBullets:
+        if len(self.game_instance.enemies) > 0:
+            self.average_enemy_x /= len(self.game_instance.enemies)
+        for bullet in self.game_instance.bullets:
             self.bullet_x.append(bullet.rect.x)
-        self.space_bot.choose_move([self.get_player_screen_position(), self.get_bullets_above_player(), self.get_enemies_position()])
+        self.Space_bot.choose_move([self.get_player_screen_position(), self.get_bullets_above_player(), self.average_enemy_x / self.game_instance.screen.get_width()])
        # self.print_update()
 
     def get_player_screen_position(self):
-        return (self.player_x -self.game_instance.screen.width/2)/(self.game_instance.screen.width/2)
+        return self.player_x / self.game_instance.screen.get_width()
 
     def get_bullets_above_player(self):
-        bullet_count = -1
+        bullet_count = 0
         for bullet_x in self.bullet_x:
-            if self.player_x - 25 < bullet_x < self.player_x + 25:
+            if self.player_x - 50 < bullet_x < self.player_x + 50:
                 bullet_count += 1
         return bullet_count
-    def get_enemies_position(self):
-      return  (self.average_enemy_x - self.player_x) / (self.game_instance.screen.width/4)
+
     def print_update(self):
         str_out = ""
         str_out += "Player X: " + str(self.player_x)
@@ -43,10 +45,4 @@ class A3C_Observer(Observer):
         str_out += "Avg X: " + str(self.average_enemy_x)
         str_out += "Bullets: " + str(self.bullet_x)
         print(str_out)
-    def end_game(self,score):
-        self.space_bot.end_game(score)
-    def send_command(self, command):
-        if self.player_x < 100 or self.player_x > self.game_instance.screen.width - 100:
-            if command == "shoot":
-                return
-        self.game_instance.command(command)
+
